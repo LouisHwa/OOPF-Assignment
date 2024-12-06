8ab8c2222e1da9c33914944aa0ae27b2976f4961
VincentOSK <oskvincent@outlook.com>
2024-12-06 15:37:03 +0800

    Pokeball class extends PokemonBall class is created

---
+++ PokemonGame/src/classes/Pokeball.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Pokeball extends PokemonBall{
+
+	public Pokeball() {
+		super("Pokeball", 0.50, 0.6); //name = Pokeball, success rate = .5, appearing rate = .6
+		
+	}
+	
+	
+
+}
